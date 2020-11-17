dd631c039e440dc6f43ab34215cb3b114dc02d06
junjun-11 <exceed-the-limit.phiten@hotmail.co.jp>
2020-11-17 18:30:16 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,16 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.all
+    @posts = Post.all.order(id: "DESC")
     # @post = Post.find(1)
     # @post = "これはコントローラーで定義したインスタンス変数を確認するための文字列です"
   end
 
-  def new
-  end
+
 
   def create
     Post.create(content: params[:content]) 
     # ここの（）括弧って引数の括弧だったのね
+    redirect_to action: :index
   end
 end
