95c17be08686c26913f4af32640fb95ff7085635
junjun-11 <exceed-the-limit.phiten@hotmail.co.jp>
2020-11-20 13:08:11 +0900

    既読機能のサーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,4 +13,16 @@ class PostsController < ApplicationController
     # ここの（）括弧って引数の括弧だったのね
     redirect_to action: :index
   end
+
+ def checked
+  post = Post.find(params[:id])
+  if post.checked 
+    post.update(checked: false)
+  else
+    post.update(checked: true)
+  end
+
+  item = Post.find(params[:id])
+  render json: { post: item }
+ end
 end
